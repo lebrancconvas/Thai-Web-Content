48557b0bb2235437d03560e4f748cca2a5332cba
lebrancconvas <lebranccconvas@gmail.com>
2019-03-01 23:41:17 +0700

    :tada: Create Repository.

---
+++ static/js/index.js
@@ -0,0 +1,11 @@
+function thematter() {
+    window.location.assign("https://thematter.co")
+}
+
+function themomentum() {
+    window.location.assign("https://themomentum.co")
+}
+
+function spaceth() {
+    window.location.assign("https://spaceth.co")
+}
